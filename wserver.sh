7e5f5bcdc92f9580d8f199cf5ff47099fd1cca01
Vukovic Nikola <admin@nikolav.rs>
2025-07-31 15:41:06 +0200

    waitress-serve

--- wserver.sh
+++ wserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python app.py
-# waitress-serve --host 0.0.0.0 --port 5000 flask_app:app
+# python app.py
+waitress-serve --host 0.0.0.0 --port 5000 app:app
 # hypercorn --bind 0.0.0.0:5000 flask_app_asgi:asgi_app
