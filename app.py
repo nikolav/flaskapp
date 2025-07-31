967e60557aa85a0276df98821e76f3cc45087e65
Vukovic Nikola <admin@nikolav.rs>
2025-07-31 15:46:56 +0200

    Thu, Jul 31, 2025  1:46:56 PM

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return f'hello {Config.MESSAGE}'
+    return f'!hello {Config.MESSAGE}!'
 
 if __name__ == "__main__":
     app.run(debug=True)
