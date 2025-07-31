34c7165807e7beaa2c08fb24fac4135f05c4ce2a
Vukovic Nikola <admin@nikolav.rs>
2025-07-31 19:20:14 +0200

    Thu, Jul 31, 2025  5:20:14 PM

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,6 @@ from flask          import Flask
 from flask_cors     import CORS
 from flask_talisman import Talisman
 
-
 from src.config import Config
 
 
@@ -10,26 +9,24 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = Config.SECRET_KEY
 
 
-# graphql:setup @[`POST /graphql`]
+# routes:setup
+#  graphql @[`POST /graphql`]
 from src.graphql.setup import graphql_mount_endpoint
 graphql_mount_endpoint(app)
 
-# routes:misc.
+#  misc.
 @app.route('/', methods=('GET',))
 def hello():
     return f'!hello {Config.MESSAGE}!'
 
 
 # cors:setup
-if Config.PRODUCTION:    
-    CORS(app, 
-        supports_credentials = True, 
-        resources = {
-            r'/graphql': {'origins': '*'},
-        }
-    )
-else:
-    CORS(app, supports_credentials = True)
+CORS(app, 
+    supports_credentials = True, 
+    resources = {
+        r'/graphql': {'origins': '*'},
+    } if Config.PRODUCTION else '',
+)
 
 
 # talisman:setup
