ec1d764b98809136d4f74f83a331e566461da419
Vukovic Nikola <admin@nikolav.rs>
2025-08-04 14:56:07 +0200

    Mon, Aug  4, 2025 12:56:07 PM

--- src/graphql/resolvers/query/demo.py
+++ src/graphql/resolvers/query/demo.py
@@ -4,7 +4,7 @@ from src.graphql.setup import query
 
 @query.field('demo')
 def resolve_demo(_o, _i):  
-  return ''
+  # return ''
   
   # import base64
   # from flask import render_template
@@ -12,8 +12,8 @@ def resolve_demo(_o, _i):
   # return printHtmlToPDF(render_template('pdf/blank-a4.html', content = 'FOO:BAR'), 
   #                       base64_encoded = True)
   
-  # from src.services.jwt import JWT
+  from src.services.jwt import JWT
   # tok = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIyMzMzLCJAIjoiMjAyNS0wOC0wMSAwOTozMjo0MS4zNjQ4MjUrMDA6MDAifQ.l4i5Q-1YRvSOTSp8_owJwDdQf5v3zmfASOWeQbe4-0I'
-  # return JWT.is_valid(tok)
+  return JWT.encode({ 'admin@nikolav.rs': '122' })
 
 
