166e569153e7ce8bad17a6549897b3e16fe3185a
Vukovic Nikola <admin@nikolav.rs>
2025-08-08 21:37:53 +0200

    Fri, Aug  8, 2025  7:37:53 PM

--- src/graphql/resolvers/query/status.py
+++ src/graphql/resolvers/query/status.py
@@ -1,19 +1,31 @@
+
 from src.graphql.setup import query
 
 
 @query.field('status')
 def resolve_status(_o, _i):
-  redis_client_version = None
+  redis_version = None
+  mongo_version = None
 
   try:
     from flask_app import redis_client
-    _err, client = redis_client
-    redis_client_version = client.info().get('redis_version')
+    from flask_app import mongo
+
+    if redis_client:
+      _err_redis, client_redis = redis_client
+      redis_version     = client_redis.info().get('redis_version') if client_redis else ''
+    
+    if mongo:
+      _err_mongo, client_mongo = mongo
+      mongo_version = client_mongo.cx.server_info()['version'] if client_mongo else ''
+        
   except:
     pass
 
   return {
     'status' : 'ok',
-    'redis'  : redis_client_version,
+    'redis'  : redis_version,
+    'mongo'  : mongo_version,
   }
+
 
