b91a886126f0639d34ccca5062b04adad6c9febf
Vukovic Nikola <admin@nikolav.rs>
2025-08-13 15:05:24 +0200

    Wed, Aug 13, 2025  1:05:24 PM

--- src/services/pdf/__init__.py
+++ src/services/pdf/__init__.py
@@ -8,13 +8,13 @@ from src.utils.text_to_uri_data import text_to_uri_data
 
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-infobars')
-chrome_options.add_argument('--disable-extensions')
-chrome_options.add_argument('--disable-popup-blocking')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-infobars')
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-popup-blocking')
 
 params = { 
   'landscape'       : False,
