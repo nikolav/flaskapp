f4789da56deb01004e2beabc0535b362e49bf4e3
Vukovic Nikola <admin@nikolav.rs>
2025-08-25 00:17:18 +0200

    route:who

--- src/blueprints/auth.py
+++ src/blueprints/auth.py
@@ -11,6 +11,20 @@ from src.schemas.validation          import SchemaAuthArguments
 from src.services.jwt                import JWT
 
 
+def _user_to_dict(user):
+  return {
+    'uid': user.uid,
+    'email': user.email,
+    'phone_number': user.phone_number,
+    'display_name': user.display_name,
+    'photo_url': user.photo_url,
+    'email_verified': user.email_verified,
+    'disabled': user.disabled,
+    'custom_claims': user.custom_claims,
+    'provider_data': [p.__dict__ for p in user.provider_data],
+    'tokens_valid_after_time': str(user.tokens_valid_after_timestamp),
+  }
+
 bp_auth = Blueprint('auth', __name__, url_prefix = '/auth')
 
 # cors blueprints for cross-domain
@@ -39,5 +53,18 @@ def resolve_route_authenticate():
       return { 'token': token }, 200
   
   return { 'error': str(error) }, 401
+
+
+@bp_auth.route('/who', methods = ('GET',))
+def auth_who():
+  error = '@error/auth:who'
+  try:
+    # send user data
+    return _user_to_dict(g.user), 200
+  
+  except Exception as err:
+    error = err
+  
+  return { 'error': str(error) }, 500
 
 
