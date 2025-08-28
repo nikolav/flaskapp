654e5a1ea0b1cb98e4fe56e8a9c8d52c9716287b
Vukovic Nikola <admin@nikolav.rs>
2025-08-28 21:29:37 +0200

    Thu, Aug 28, 2025  7:29:37 PM

--- src/services/pwd/__init__.py
+++ src/services/pwd/__init__.py
@@ -11,8 +11,8 @@ class PWD:
     return base64.b64encode(bPasswordHashed).decode('utf-8')
   
   @staticmethod
-  def verify(password, b64_hashed_password):
+  def verify(password, b64_password):
     bPassword       = password.encode('utf-8')
-    bPasswordHashed = base64.b64decode(b64_hashed_password.encode('utf-8'))
+    bPasswordHashed = base64.b64decode(b64_password.encode('utf-8'))
     return bcrypt.checkpw(bPassword, bPasswordHashed)
   
