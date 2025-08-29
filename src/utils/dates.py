aa15786662295fe9ba3c719be0548f48ed1c4ec8
Vukovic Nikola <admin@nikolav.rs>
2025-08-29 03:26:13 +0200

    Fri, Aug 29, 2025  1:26:13 AM

--- src/utils/dates.py
+++ src/utils/dates.py
@@ -1,9 +1,19 @@
 
 from datetime import datetime
 from datetime import timezone
+# from typing   import Optional
+# from zoneinfo import ZoneInfo
+
+
+DATE_FORMATS = {
+  # REST/JSON
+  'ISO-8601': '%Y-%m-%dT%H:%M:%SZ',
+  # headers/cookies
+  'RFC-1123': '%a, %d %b %Y %H:%M:%S GMT',
+}
 
 def utcnow():
-  return datetime.now(tz = timezone.utc)
+  return datetime.now(timezone.utc)
 
 def with_doc_timestamps(doc, *, 
     field_created_at = 'created_at', 
@@ -12,4 +22,7 @@ def with_doc_timestamps(doc, *,
   doc.setdefault(field_created_at, tt)
   doc[field_updated_at] = tt
   return doc
+
+def to_utc(dt: datetime):
+  return dt.replace(tzinfo = timezone.utc) if dt.tzinfo is None else dt.astimezone(timezone.utc)
 
