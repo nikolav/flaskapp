673b3a27b19e48f5c1ebe67c3e5858fc2e82264d
Vukovic Nikola <admin@nikolav.rs>
2025-11-28 22:24:59 +0100

    Fri, Nov 28, 2025  9:24:59 PM

--- src/graphql/resolvers/query/status.py
+++ src/graphql/resolvers/query/status.py
@@ -18,14 +18,14 @@ def resolve_status(_o, _i):
     from flask_app import mongo
     from flask_app import db
 
-    if redis_client:
-      _err_redis, client_redis = redis_client
-      redis_version = client_redis.info().get('redis_version') if client_redis else ''
-    
     if mongo:
       _err_mongo, client_mongo = mongo
       mongo_version = client_mongo.cx.server_info()['version'] if client_mongo else ''
-    
+
+    if redis_client:
+      _err_redis, client_redis = redis_client
+      redis_version = client_redis.info().get('redis_version') if client_redis else ''
+        
     if db:
       _err_db, client_db = db
       db_version = client_db.session.execute(
