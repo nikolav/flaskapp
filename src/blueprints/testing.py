6685fa34474b9bd52384d34c4d01f6ba4301ec0c
Vukovic Nikola <admin@nikolav.rs>
2025-12-11 00:33:33 +0100

    Wed, Dec 10, 2025 11:33:33 PM

--- src/blueprints/testing.py
+++ src/blueprints/testing.py
@@ -10,4 +10,16 @@ CORS(bp_testing)
 
 @bp_testing.route('/', methods = ('POST',))
 def resolve_route_testing():
-  return []
+  from src.services.cache import Cache
+  from src.utils          import Utils
+
+  r     = Utils.ResponseStatus()
+  token = 'foo:1'
+
+  try:
+    r.status = { token: Cache.key(token) }
+
+  except Exception as e:
+    r.error = e
+
+  return r.dump()
